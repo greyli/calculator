f9e6c4ed206b5433482bc4bc408d51bf2ea79da8
Hui Li <withlihui@gmail.com>
2016-12-06 11:57:54 +0800

    v1.0

---
+++ README.md
@@ -0,0 +1,7 @@
+# Calculator
+A simple calculator made with JavaScript.
+
+## DEMO
+http://codepen.io/greyli/full/bBYYJJ/
+
+![demo](https://raw.githubusercontent.com/greyli/js-calculator/master/image/demo.png)
---
+++ js/main.js
@@ -0,0 +1,103 @@
+$(document).ready(function() {
+  var clearData = function() {
+    $('#num1').val('');
+    $('#operator').val('');
+    $('#num2').val('');
+    $('#temp').val('');
+  };
+
+  var clearOutput = function() {
+    $('#output').html('');
+    $('#output2').html('');
+  };
+
+  $('.nums').click(function() {
+    if (('+-*/').indexOf($('#output').html()) != -1) {
+      $('#output').html('');
+    }
+
+    if ($('#output').html() == '0' || $('#output2').html() == 'Reach Digit Limit') {
+      clearOutput()
+    }
+
+    if ($('#temp').val() !== '') {
+      clearOutput()
+      clearData();
+    }
+
+    $('#output').append($(this).val());
+    $('#output2').append($(this).val());
+    if ($('#output').html().length > 12) {
+      $('#output').html('0');
+      $('#output2').html('Reach Digit Limit');
+    }
+  });
+
+  $('#clearButton').click(function() {
+    $('#output').html('0');
+    $('#output2').html('');
+    clearData();
+  });
+
+  $('.btn-operate').click(function() {
+    var operator = $(this).val();
+    if ($('#num1').val() !== '' && $('#operator').val() !== '') {
+      $('#num2').val($('#output').html());
+      var num1 = parseFloat($('#num1').val());
+      var operator = $('#operator').val();
+      var num2 = parseFloat($('#num2').val());
+      var result;
+      if (operator == '+') {
+        result = num1 + num2;
+      } else if (operator == '-') {
+        result = num1 - num2;
+      } else if (operator == '*') {
+        result = num1 * num2;
+      } else if (operator == '/') {
+        result = parseFloat(num1 / num2);
+      }
+        if (result.toString().length > 16) {
+          $('#output').html('0');
+          $('#output2').html('Reach Digit Limit');
+          $('#temp').val(0);
+        } else {
+          $('#output').html(operator);
+          $('#output2').append(operator);
+          $('#num1').val(result);
+          $('#operator').val(operator);
+        }
+
+    } else {
+      $('#num1').val($('#output').html());
+      $('#operator').val($(this).val());
+      $('#output').html(operator);
+      $('#output2').append($(this).val());
+    }
+  });
+
+  $('#resultButton').click(function() {
+    $('#num2').val($('#output').html());
+     var num1 = parseFloat($('#num1').val());
+      var operator = $('#operator').val();
+      var num2 = parseFloat($('#num2').val());
+      var result;
+      if (operator == '+') {
+        result = num1 + num2;
+      } else if (operator == '-') {
+        result = num1 - num2;
+      } else if (operator == '*') {
+        result = num1 * num2;
+      } else if (operator == '/') {
+        result = parseFloat(num1 / num2);
+      }
+      if (result.toString().length > 16) {
+        $('#output').html('0');
+        $('#output2').html('Reach Digit Limit');
+        $('#temp').val(0);
+      } else {
+        $('#output').html(result);
+        $('#output2').html('');
+        $('#temp').val(result);
+      }
+  });
+});
