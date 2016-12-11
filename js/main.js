a04b5586e212f50ce0c8ac5e78fa686ce83ce71d
Hui Li <withlihui@gmail.com>
2016-12-11 16:49:48 +0800

    add delete button

--- js/main.js
+++ js/main.js
@@ -1,104 +1,121 @@
 $(document).ready(function() {
-  var clearData = function() {
-    $('#num1').val('');
-    $('#operator').val('');
-    $('#num2').val('');
-    $('#temp').val('');
-  };
+    var mainOutput = $('#output');
+    var subOutput = $('#output2');
+    var op = $('#operator');
+    var num2 = $('#num2');
+    var num1 = $('#num1');
+    var temp = $('#temp');
+    var clearData = function() {
+        num1.val('');
+        op.val('');
+        num2.val('');
+        temp.val('');
+    };
 
-  var clearOutput = function() {
-    $('#output').html('');
-    $('#output2').html('');
-  };
+    var clearOutput = function() {
+        mainOutput.html('');
+        subOutput.html('');
+    };
 
-  $('.nums').click(function() {
-    if (('+-*/').indexOf($('#output').html()) != -1) {
-      $('#output').html('');
+    var digitError = function() {
+        mainOutput.html('0');
+        subOutput.html('Reach Digit Limit');
+        temp.val(0);
     }
 
-    if ($('#output').html() == '0' || $('#output2').html() == 'Reach Digit Limit') {
-      clearOutput()
-    }
+    $('.nums').click(function() {
+        if (('+-*/').indexOf(mainOutput.html()) != -1) {
+            mainOutput.html('');
+        }
 
-    if ($('#temp').val() !== '') {
-      clearOutput()
-      clearData();
-    }
+        if (mainOutput.html() == '0' || subOutput.html() == 'Reach Digit Limit') {
+            clearOutput()
+        }
 
-    $('#output').append($(this).val());
-    $('#output2').append($(this).val());
-    if ($('#output').html().length > 12) {
-      $('#output').html('0');
-      $('#output2').html('Reach Digit Limit');
-    }
-  });
+        if (temp.val() !== '') {
+            clearOutput()
+            clearData();
+        }
 
-  $('#clearButton').click(function() {
-    $('#output').html('0');
-    $('#output2').html('');
-    clearData();
-  });
+        mainOutput.append($(this).val());
+        subOutput.append($(this).val());
 
-  $('.btn-operate').click(function() {
+        if (mainOutput.html().length > 12) {
+            digitError();
+        }
+    });
+
+    $('#clearButton').click(function() {
+        mainOutput.html('0');
+        subOutput.html('');
+        clearData();
+    });
+
+    $('#deleteButton').click(function() {
+        if (mainOutput.html() != '0') {
+            mainOutput.html(mainOutput.html().substring(0, mainOutput.html().length-1));
+            subOutput.html(subOutput.html().substring(0, subOutput.html().length-1));
+            if (mainOutput.html() == '') {
+                mainOutput.html('0');
+            }
+        }
+    });
+
+    $('.btn-operate').click(function() {
     var newOperator = $(this).val();
-    if ($('#num1').val() !== '' && $('#operator').val() !== '') {
-      $('#num2').val($('#output').html());
-      var num1 = parseFloat($('#num1').val());
-      var operator = $('#operator').val();
-      var num2 = parseFloat($('#num2').val());
-      var result;
-      if (operator == '+') {
-        result = num1 + num2;
-      } else if (operator == '-') {
-        result = num1 - num2;
-      } else if (operator == '*') {
-        result = num1 * num2;
-      } else if (operator == '/') {
-        result = parseFloat(num1 / num2);
-      }
+    if (num1.val() !== '' && op.val() !== '') {
+          num2.val(mainOutput.html());
+          var number1 = parseFloat(num1.val());
+          var operator = op.val();
+          var number2 = parseFloat(num2.val());
+          var result;
+          if (operator == '+') {
+              result = number1 + number2;
+          } else if (operator == '-') {
+              result = number1 - number2;
+          } else if (operator == '*') {
+              result = number1 * number2;
+          } else if (operator == '/') {
+              result = parseFloat(number1 / number2);
+          }
 
-      if (result.toString().length > 12) {
-        $('#output').html('0');
-        $('#output2').html('Reach Digit Limit');
-        $('#temp').val(0);
-      } else {
-        $('#output').html(newOperator);
-        $('#output2').html(result + newOperator);
-        $('#num1').val(result);
-        $('#operator').val(newOperator);
-      }
-
+          if (result.toString().length > 12) {
+              digitError();
+          } else {
+              mainOutput.html(newOperator);
+              subOutput.html(result + newOperator);
+              num1.val(result);
+              op.val(newOperator);
+          }
     } else {
-      $('#num1').val($('#output').html());
-      $('#operator').val(newOperator);
-      $('#output').html(newOperator);
-      $('#output2').append(newOperator);
+        num1.val(mainOutput.html());
+        op.val(newOperator);
+        mainOutput.html(newOperator);
+        subOutput.append(newOperator);
     }
-  });
+    });
 
-  $('#resultButton').click(function() {
-    $('#num2').val($('#output').html());
-     var num1 = parseFloat($('#num1').val());
-      var operator = $('#operator').val();
-      var num2 = parseFloat($('#num2').val());
-      var result;
-      if (operator == '+') {
-        result = num1 + num2;
-      } else if (operator == '-') {
-        result = num1 - num2;
-      } else if (operator == '*') {
-        result = num1 * num2;
-      } else if (operator == '/') {
-        result = parseFloat(num1 / num2);
-      }
-      if (result.toString().length > 12) {
-        $('#output').html('0');
-        $('#output2').html('Reach Digit Limit');
-        $('#temp').val(0);
-      } else {
-        $('#output').html(result);
-        $('#output2').html('');
-        $('#temp').val(result);
-      }
-  });
+    $('#resultButton').click(function() {
+        num2.val(mainOutput.html());
+        var number1 = parseFloat(num1.val());
+        var operator = op.val();
+        var number2 = parseFloat(num2.val());
+        var result;
+        if (operator == '+') {
+            result = number1 + number2;
+        } else if (operator == '-') {
+            result = number1 - number2;
+        } else if (operator == '*') {
+            result = number1 * number2;
+        } else if (operator == '/') {
+            result = parseFloat(number1 / number2);
+        }
+        if (result.toString().length > 12) {
+            digitError();
+        } else {
+            mainOutput.html(result);
+            subOutput.html('');
+            temp.val(result);
+        }
+    });
 });
